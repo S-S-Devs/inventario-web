3b14248b9b76f25b2d9ada52f9f6e5cbc70f069f
JavierUnexca <lbpf2021jesr@gmail.com>
2025-02-23 19:40:24 -0400

    Resgistro y Consulta

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
+
 function handleAction(action) {
   switch(action) {
       case 'registro':
-          alert('Registro seleccionado');
+          document.getElementById('Menu').style.display = 'none';
+          document.getElementById('registro').style.display = 'block';
           break;
       case 'consulta':
-          alert('Consulta seleccionada');
+            document.getElementById('Menu').style.display = 'none';
+            document.getElementById('consulta').style.display = 'block';
           break;
       case 'modificar':
           alert('Modificar seleccionado');
           break;
-      case 'listar':
-          alert('Listar seleccionado');
+      case 'eliminar':
+          alert('Eliminar seleccionado');
           break;
       case 'salir':
-          alert('Salir seleccionado');
-          break;
+        alert('Salir seleccionado');
+        break;
+      case 'volver':
+        if (menu.style.display === 'none' && registro.style.display === 'block') {
+            document.getElementById('registro').style.display = 'none';
+            document.getElementById('Menu').style.display = 'block';
+            break;
+        } else if (menu.style.display === 'none' && consulta.style.display === 'block') {
+            document.getElementById('consulta').style.display = 'none';
+            document.getElementById('Menu').style.display = 'block';
+            break;
+        }
       default:
           alert('Acción no reconocida');
   }
